96f5ec5f17cacbdfd29f72854e8211bbfdfbb6aa
VanMinh <53778428+vanminh1310@users.noreply.github.com>
2021-01-19 22:17:12 +0700

    sua lai
    dfdf

--- freertos/freertos.ino
+++ freertos/freertos.ino
@@ -1,26 +1,54 @@
-#include "main.h"
+#include "main.h" // chuyển qua bên main.h để đổi 
 TaskHandle_t Task1;
 TaskHandle_t Task2;
 
-
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(ledgr, OUTPUT);
-  pinMode(ledred, OUTPUT);
-
-  dht.begin();
-
+int i = 0;
+unsigned long myTime;
+//kiem tra ket noi
+void wifi() {
   WiFi.begin(WIFI_SSID , WIFI_PASSWORD);
 
   while ( WiFi.status() != WL_CONNECTED ) {
+    i++;
+    //    delay(1000);
+    //    Serial.println(i);
     digitalWrite(ledred, HIGH);
     delay ( 500 );
     digitalWrite(ledred, LOW);
     delay ( 500 );
     Serial.print ( "." );
+    if (i == 10) {
+      i = 0;
+      break;
+
+    }
   }
-  digitalWrite(ledgr, HIGH);
+
+
+
+}
+
+void setup() {
+  Serial.begin(9600);
+  myTime = millis();
+  pinMode(ledgr, OUTPUT);
+  pinMode(ledred, OUTPUT);
+
+  dht.begin();
+
+  wifi();
+  //trang thai wifi
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    digitalWrite(ledred, HIGH);
+    digitalWrite(ledgr, LOW);
+  }
+
+  else {
+    digitalWrite(ledgr, HIGH);
+    digitalWrite(ledred, LOW);
+  }
+
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
   xTaskCreatePinnedToCore(
@@ -28,11 +56,11 @@ void setup() {
     "Task1",     /* name of task. */
     10000,       /* Stack size of task */
     NULL,        /* parameter of the task */
-    5,           /* priority of the task  độ ưu tiên*/ 
+    5,           /* priority of the task  độ ưu tiên*/
     &Task1,      /* Task handle to keep track of created task */
     0);          /* pin task to core 0 */
 
-delay(500);
+  delay(500);
   //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
   xTaskCreatePinnedToCore(
     Task2code,   /* Task function. */
@@ -62,8 +90,20 @@ void Task1code( void * pvParameters ) {
     float t = dht.readTemperature();
     Firebase.setFloat(firebaseData, "Floors1/T", t);
     Firebase.setFloat(firebaseData, "Floors1/H", h);
-    //
-   
+    // neu nhu k co ket noi wifi thì kiem tra lại 
+
+    if (WiFi.status() != WL_CONNECTED) {
+
+      digitalWrite(ledred, HIGH);
+      digitalWrite(ledgr, LOW);
+      wifi();
+    }
+    else {
+      digitalWrite(ledgr, HIGH);
+      digitalWrite(ledred, LOW);
+    }
+
+
 
   }
 
@@ -79,7 +119,7 @@ void Task2code( void * pvParameters ) {
   for (;;) {
     float h = dht.readHumidity();
     float t = dht.readTemperature();
-     Serial.print("Nhiet do: ");
+    Serial.print("Nhiet do: ");
     Serial.println(t);
     Serial.print("Do am: ");
     Serial.println(h);
@@ -98,7 +138,7 @@ void Task2code( void * pvParameters ) {
     Serial.print(fire_digital);
     Serial.println("");
 
-    if (gas_digital == 0||fire_digital == 0)
+    if (gas_digital == 0 || fire_digital == 0)
 
     {
 
@@ -107,11 +147,20 @@ void Task2code( void * pvParameters ) {
     }
 
 
+
+
   }
 
- 
+
 }
 
 void loop() {
+
+
+
+
+
+
+
 
 }
